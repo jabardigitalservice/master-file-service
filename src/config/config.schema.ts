8e3df00213c3614b434ec7a928fa4af1376404fa
ayocodingit <ayocodingit@gmail.com>
2023-12-04 11:44:19 +0700

    feat(images): add db mongo

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -8,4 +8,10 @@ export default Joi.object({
     APP_PORT_HTTP: Joi.number().required(),
     APP_LOG: Joi.string().valid('info', 'error', 'warn').required(),
     FILE_URL: Joi.string().uri().optional(),
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().required(),
+    DB_USERNAME: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+    DB_NAME: Joi.string().required(),
+    DB_AUTH_SOURCE: Joi.string().optional(),
 })
---
+++ src/database/seeds/logo.seed.ts
@@ -0,0 +1,41 @@
+import config from '../../config/config'
+import { GetFileUrl } from '../../helpers/url'
+import Logger from '../../pkg/logger'
+import { ListLogo } from '../constant/logo'
+import Mongo from '../mongo/mongo'
+import imageSchema from '../mongo/schemas/image.schema'
+
+const run = async () => {
+    const logger = new Logger(config)
+    await Mongo.Connect(logger, config)
+
+    const image = imageSchema
+
+    const images = []
+    for (const { title, filename } of ListLogo) {
+        const path = 'logos/' + filename
+        const category = 'logo'
+
+        const data = {
+            title,
+            file: {
+                path,
+                size: 0,
+                mimetype: 'image/svg+xml',
+                originalname: filename,
+                filename,
+                uri: GetFileUrl(config.file.url, path),
+            },
+            category,
+            tags: [title, category],
+        }
+
+        images.push(image.updateOne({ title }, data, { upsert: true }))
+    }
+
+    await Promise.all(images)
+
+    await Mongo.Disconnect()
+}
+
+export default run()
