12247349e2a2de01e94ebe64f9b7ff98e9ec8699
ayocodingit <ayocodingit@gmail.com>
2023-12-05 09:10:13 +0700

    feat(image): add filter by keyword and category

--- src/modules/images/repository/mongo/repository.ts
+++ src/modules/images/repository/mongo/repository.ts
@@ -5,9 +5,23 @@ import imageSchema from '../../../../database/mongo/schemas/image.schema'
 class Repository {
     constructor(private logger: Logger) {}
 
-    public async Fetch({ offset, limit }: RequestParams) {
-        const data = await imageSchema.find().skip(offset).limit(limit)
-        const count = await imageSchema.find().count()
+    public async Fetch({ offset, limit, keyword, category }: RequestParams) {
+        const filter = {}
+
+        if (keyword)
+            Object.assign(filter, {
+                title: {
+                    $regex: new RegExp(keyword, 'i'),
+                },
+            })
+
+        if (category)
+            Object.assign(filter, {
+                category: category,
+            })
+            
+        const data = await imageSchema.find(filter).skip(offset).limit(limit)
+        const count = await imageSchema.find(filter).count()
 
         return {
             data,
